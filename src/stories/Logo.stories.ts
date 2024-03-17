e3cff0037b262f9f5a9ac54b9d72ca710d2df1b1
paring <me@paring.moe>
2024-03-17 14:34:17 +0900

    feat: add Logo component

---
+++ src/stories/Logo.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/svelte'
+import { Logo } from '$lib'
+
+const meta = {
+	// @ts-expect-error type issue
+	component: Logo
+} satisfies Meta<typeof Logo>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+	args: {
+		width: 480
+	}
+}
