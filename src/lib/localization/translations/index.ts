7ff02bb64943530f71d9dfc51d228e5323807c46
paring <me@paring.moe>
2024-08-14 17:14:46 +0900

    feat: generate imports script

--- src/lib/localization/translations/index.ts
+++
@@ -1,7 +0,0 @@
-const files = Object.fromEntries(
-	Object.entries(
-		import.meta.glob('./*/*.ftl', { eager: true, query: 'raw', import: 'default' })
-	).map(([k, v]) => [k.slice(2, k.length - 4), v])
-)
-
-export const translationData: Record<string, string> = files as Record<string, string>
