a47654b9e508940924e7ca33e9fd87acb2caa1fa
paring <me@paring.moe>
2024-09-23 14:07:06 +0900

    feat: grid util class

---
+++ src/stories/utils/Grid.stories.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" context="module">
+	import { defineMeta } from '@storybook/addon-svelte-csf'
+
+	const { Story } = defineMeta({
+		title: 'Utils/Grid'
+	})
+</script>
+
+<Story name="Default">
+	<div class="grid">
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+		<div class="col"></div>
+	</div>
+</Story>
+
+<style lang="scss">
+	.col {
+		height: 48px;
+		background-color: rgba(255, 0, 0, 0.4);
+	}
+</style>
