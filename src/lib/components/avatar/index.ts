ae84c4eb88d4284e9c1e36a959b924f1e5309f45
paring <paringchan@proton.me>
2025-11-01 03:15:24 +0900

    feat: avatar

---
+++ src/lib/components/avatar/index.ts
@@ -0,0 +1,2 @@
+export { default as Avatar } from './component.svelte';
+export type { AvatarProps } from './component.svelte';
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 import './global.css';
 
 export * from './components/button/index.js';
+export * from './components/avatar/index.js';
