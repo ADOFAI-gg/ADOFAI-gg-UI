bf704fb76fbfbad2a325ed5e4112e80a2958859c
paring <paringchan@proton.me>
2025-11-03 01:01:39 +0900

    refactor: group nav menu to another directory

--- src/lib/components/nav/index.ts
+++ src/lib/components/nav/index.ts
@@ -1,4 +1,4 @@
 export { default as Root } from './root.svelte';
 export { default as Link } from './link.svelte';
-export { default as MenuItem } from './menu-item.svelte';
+export { default as MenuItem } from './menu/menu-item.svelte';
 export * from './props.js';
