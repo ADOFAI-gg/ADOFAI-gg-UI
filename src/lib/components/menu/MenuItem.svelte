7213f2c4243895d50f1abee295f60fb69d366ad4
imnyang <imnyang@pm.me>
2026-08-17 02:39:46 +0900

    feat: migrate generic menu item

---
+++ src/lib/components/menu/MenuItem.svelte
@@ -0,0 +1,51 @@
+<script lang="ts" module>
+	import { DropdownMenu } from 'bits-ui';
+	import type { Snippet } from 'svelte';
+
+	export type MenuItemProps = Omit<
+		DropdownMenu.ItemProps,
+		'child' | 'children' | 'onclick' | 'onSelect'
+	> & {
+		onclick?: () => void;
+		icon?: Snippet;
+		children?: Snippet;
+		noIcon?: boolean;
+		variant?: 'default' | 'danger';
+	};
+</script>
+
+<script lang="ts">
+	import { cn } from '$lib/utils/ui.js';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		onclick,
+		icon,
+		children,
+		noIcon = false,
+		variant = 'default',
+		...restProps
+	}: MenuItemProps = $props();
+</script>
+
+<DropdownMenu.Item
+	bind:ref
+	{...restProps}
+	class={cn(
+		'h-8 gap-4 flex min-w-[180px] cursor-pointer items-center bg-transparent transition-colors',
+		variant === 'danger'
+			? 'text-gg-red data-[highlighted]:bg-gg-red/20'
+			: 'text-white data-[highlighted]:bg-white/20',
+		className
+	)}
+	onSelect={() => onclick?.()}
+>
+	{#if !noIcon}
+		<div class="size-4 shrink-0">
+			{@render icon?.()}
+		</div>
+	{/if}
+
+	{@render children?.()}
+</DropdownMenu.Item>
